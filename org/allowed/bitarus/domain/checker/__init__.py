f2cde77c0448e4786fe8e3bb9a7a3001529ed584
Vitor Borrego <vitor.mln.borrego@gmail.com>
2013-10-09 23:01:43 +0100

    expiration_date may return a list of items. handler for it created.

--- org/allowed/bitarus/domain/checker/__init__.py
+++ org/allowed/bitarus/domain/checker/__init__.py
@@ -21,7 +21,7 @@ class Logger(object):
     '''
     @staticmethod
     def log(msg):
-        syslog.syslog("[domainChecker] %s"%(msg))
+        syslog.syslog("[domainChecker] %s" % (msg))
         
 class SendMail(object):
     '''
@@ -29,9 +29,9 @@ class SendMail(object):
     '''
     def __init__(self, config, subject, message):
         s = smtplib.SMTP()
-        header =  'From:%s\nTo:%s\nSubject:%s\n\n'%(config.getMailFrom(),config.getMailTo(),subject)
+        header = 'From:%s\nTo:%s\nSubject:%s\n\n' % (config.getMailFrom(), config.getMailTo(), subject)
         try:
-            (code, response) = s.connect(config.getServer() , config.getPort() )  # expects 220
+            (code, response) = s.connect(config.getServer() , config.getPort())  # expects 220
             if code == 220: 
                 (code, response) = s.ehlo('domainChecker')  # expects 250
             else:
@@ -41,44 +41,46 @@ class SendMail(object):
             else:
                 raise Exception('Ehlo failed')
             if code == 220: 
-                (code, response) = s.login(config.getUser(), config.getPassword() )  # expects 235
+                (code, response) = s.login(config.getUser(), config.getPassword())  # expects 235
             else:
                 raise Exception('StartTLS failed')                
             if code == 235: 
-                Logger.log("Before sendmail:%s"%(message)  )
-                (code, response) = s.sendmail(config.getMailFrom(), config.getMailTo(), "%s%s"%(header,message))
+                Logger.log("Before sendmail:%s" % (message))
+                (code, response) = s.sendmail(config.getMailFrom(), config.getMailTo(), "%s%s" % (header, message))
             else:
                 raise Exception('Login failed')            
         except Exception as e:
-            Logger.log("SendMail exception: %s" % (e) ) 
+            Logger.log("SendMail exception: %s" % (e)) 
         s.quit()
         
 class Domain(object):
     '''
     Stores info about domain expiration
     '''
-    def __init__(self, domain, status, expirationDate):
+    def __init__(self, domain, status, expirationDate, text):
         self.exception = ''
         try:  # may give an error if no expiration date is found in whois registry
+            #print expirationDate
             currTime = time.time()
             self.domain = domain
             self.status = status        
+            self.text = text
             self.expirationDate = expirationDate                
             self.expireDays = expirationDate - datetime.datetime.fromtimestamp(currTime)
         except Exception as ex:
             self.expireDays = 0
             self.expirationDate = None
-            self.exception = ex.message
+            self.exception = str(ex)
     def __repr__(self):
         if len(self.exception) == 0:
-            return "Domain:%s\nStatus:%s\nExpiration date: %s\nExpires in: %s\n\n" % (self.domain,self.status, self.expirationDate, self.expireDays)
+            return "Domain:%s\nStatus:%s\nExpiration date: %s\nExpires in: %s\n\n" % (self.domain, self.status, self.expirationDate, self.expireDays)
         else:
-            return "Domain:%s\nException: %s\n\n" % (self.domain, self.exception)
+            return "Domain:%s\nStatus:%s\nExpiration date: %s\nText: %s\nException: %s\n\n" % (self.domain, self.status, self.expirationDate, self.text, self.exception)
 
 class Config(object):
     def __init__(self):
         homeDir = expanduser("~")  # get current user home dir
-        configFilePath = os.path.join(homeDir,'.domainChecker.plist')
+        configFilePath = os.path.join(homeDir, '.domainChecker.plist')
         config = plistlib.readPlist(configFilePath)  # load domains to check
         self.domains = config['domains']
         self.server = config['mailSettings']['server']
@@ -113,20 +115,29 @@ class Config(object):
                            'from':'test@example.net',
                            'to':'tset@example.net' 
         }
-        plistlib.writePlist(x,'dummyConfig.plist')
+        plistlib.writePlist(x, 'dummyConfig.plist')
         
 class Checker(object):    
     def __init__(self):
         config = Config()
         domainStatus = []
         for domain in config.getDomains():
-            whoisRes = whois.whois(domain)            
-            domainStatus.append(Domain(whoisRes.domain_name, whoisRes.status ,whoisRes.expiration_date))
+            whoisRes = whois.whois(domain)
+            # a list of expire dates may be created with multiple types in it (datetime.datetime or string)
+            expireDate = None
+            if type(whoisRes.expiration_date) is list:
+                for item in whoisRes.expiration_date:
+                    if type(item) is datetime.datetime:
+                        expireDate = item   
+            else:
+                expireDate = whoisRes.expiration_date
+                
+            domainStatus.append(Domain(whoisRes.domain_name, whoisRes.status , expireDate, str(whoisRes)))
             
         # send the results to the define email
         msg = ''
         for status in domainStatus:
             msg = "%s%s\n" % (msg , status)    
         Logger.log(msg)
-        SendMail(config,'Domain check', msg)
+        SendMail(config, 'Domain check', msg)
         
